6d0a54aaa287f960f5e3982b6ce9b8fcb7c9f459
fmoraless <franticzero@gmail.com>
2023-06-25 20:07:58 -0400

    chore: add express base conf

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application } from 'express'
+
+class App {
+	readonly expressApp: Application
+
+	constructor() {
+		this.expressApp = express()
+	}
+
+	mountHealthCheck() {
+		this.expressApp.use('/')
+	}
+}
+
+export default new App().expressApp
