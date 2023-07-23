ca409d9aa30e36c9af7e70e28d8058d93e04e16d
fmoraless <franticzero@gmail.com>
2023-07-23 02:30:31 -0400

    feat: user insert method

--- src/modules/user/infraestructure/user.infraestructure.ts
+++ src/modules/user/infraestructure/user.infraestructure.ts
@@ -1,16 +1,32 @@
 import User from '../domain/user'
 import { UserRepository } from '../domain/user.repository'
+import { UserEntity } from './user.entity'
+import DatabaseBootstrap from '../../../bootstrap/database.bootstrap'
 
 export default class UserInfraestructure implements UserRepository {
+	async insert(user: User): Promise<User> {
+		const userInsert = new UserEntity()
+		const { guid, name, lastname, email, password, active } = user.properties()
+		Object.assign(userInsert, {
+			guid,
+			name,
+			lastname,
+			email: email.value,
+			password,
+			active,
+		})
+		await DatabaseBootstrap.dataSource.getRepository(UserEntity).save(userInsert)
+
+		return user
+	}
+
 	list(): Promise<User[]> {
 		throw new Error('Method not implemented.')
 	}
 	listOne(guid: string): Promise<User> {
 		throw new Error('Method not implemented.')
 	}
-	insert(user: User): Promise<User> {
-		throw new Error('Method not implemented.')
-	}
+
 	update(user: User): Promise<User> {
 		throw new Error('Method not implemented.')
 	}
