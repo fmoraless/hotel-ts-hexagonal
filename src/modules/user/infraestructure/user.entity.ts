1d0f30d9b33d5f8cb23567ffc308a2bfdf078884
fmoraless <franticzero@gmail.com>
2023-07-23 02:29:21 -0400

    feat: create user infraestructure

---
+++ src/modules/user/infraestructure/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm'
+
+@Entity()
+export class UserEntity {
+	@PrimaryColumn()
+	guid: string
+
+	@Column({ type: 'varchar', length: 100 })
+	name: string
+
+	@Column({ type: 'varchar', length: 100 })
+	lastname: string
+
+	@Column({ type: 'varchar', length: 100 })
+	email: string
+
+	@Column({ type: 'varchar', length: 100 })
+	password: string
+
+	@Column({ type: 'boolean', default: true })
+	active: boolean
+}
