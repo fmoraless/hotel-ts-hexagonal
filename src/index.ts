d3311bcc769dfa9f37bb3d9d7952d3a0f9acfa92
fmoraless <franticzero@gmail.com>
2023-07-22 21:03:45 -0400

    chore: add mysql database connection

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import ServerBootstrap from './bootstrap/server.bootstrap'
+import DatabaseBootstrap from './bootstrap/database.bootstrap'
 import { Bootstrap } from './bootstrap/base.bootstrap'
 import Application from './app'
 
 const serverBootstrap: Bootstrap = new ServerBootstrap(Application)
+const databaseBootstrap: Bootstrap = new DatabaseBootstrap()
 
 ;(async () => {
 	try {
-		const resultServer = await serverBootstrap.initialize()
-		console.log(resultServer)
+		await serverBootstrap.initialize()
+		await databaseBootstrap.initialize()
+		console.log('Server started successfully')
 	} catch (error) {
 		console.log(error)
 	}
