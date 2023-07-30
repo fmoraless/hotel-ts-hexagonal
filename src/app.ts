354e6b34929058727ab419ef0824998923df7841
fmoraless <franticzero@gmail.com>
2023-07-30 17:36:57 -0400

    feat: implement owaps

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,33 @@ import express, { Application } from 'express'
 import routerHealth from './helpers/health'
 import HandlerErrors from './helpers/errors'
 import routerUser from './modules/user/interfaces/http/user.routes'
+import hpp from 'hpp'
+import helmet from 'helmet'
+import cors from 'cors'
+import compression from 'compression'
 
 class App {
 	readonly expressApp: Application
 
 	constructor() {
 		this.expressApp = express()
+		this.owaspSecurityMiddlewares()
 		this.mountHealthCheck()
 		this.mountMiddlewares()
 		this.mountRoutes()
 		this.mountErrors()
+	}
+
+	owaspSecurityMiddlewares() {
+		this.expressApp.use(hpp())
+		this.expressApp.use(helmet())
+		this.expressApp.use(
+			cors({
+				origin: '*',
+				optionsSuccessStatus: 200,
+				methods: ['GET', 'POST', 'PUT', 'DELETE'],
+			}),
+		)
 	}
 
 	mountHealthCheck() {
@@ -19,6 +36,7 @@ class App {
 	}
 
 	mountMiddlewares() {
+		this.expressApp.use(compression())
 		this.expressApp.use(express.json())
 		this.expressApp.use(express.urlencoded({ extended: true }))
 	}
