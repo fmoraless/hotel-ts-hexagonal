c0bbf8b6efe05714d64bed00f1de9a74b03e5f92
fmoraless <franticzero@gmail.com>
2023-07-29 22:40:48 -0400

    feat: implements validation on user factory

--- src/modules/user/domain/user.factory.ts
+++ src/modules/user/domain/user.factory.ts
@@ -2,9 +2,37 @@ import { v4 as uuidv4 } from 'uuid'
 import User, { UserProperties } from './user'
 import { UserPasswordService } from './services/user-password.service'
 import { EmailVO } from './value-objects/email.VO'
+import {
+	UserLastNameRequireException,
+	UserNameRequireException,
+	UserPasswordRequiredException,
+	UserPasswordLengthInvalidException,
+} from './exceptions/user.exception'
+import { err, ok, Result } from 'neverthrow'
+
+export type UserResult = Result<
+	User,
+	| UserNameRequireException
+	| UserLastNameRequireException
+	| UserPasswordRequiredException
+	| UserPasswordLengthInvalidException
+>
 
 export default class UserFactory {
-	async create(name: string, lastname: string, email: EmailVO, password: string) {
+	async create(name: string, lastname: string, email: EmailVO, password: string): Promise<UserResult> {
+		if (!name || name.trim() === '') {
+			return err(new UserNameRequireException())
+		}
+		if (!lastname || lastname.trim() === '') {
+			return err(new UserLastNameRequireException())
+		}
+		if (!password || password.trim() === '') {
+			return err(new UserPasswordRequiredException())
+		}
+		if (password.length < 5) {
+			return err(new UserPasswordLengthInvalidException(password))
+		}
+
 		const passwordHash = await UserPasswordService.hash(password)
 
 		const userProperties: UserProperties = {
@@ -16,6 +44,6 @@ export default class UserFactory {
 		}
 
 		const user = new User(userProperties)
-		return user
+		return ok(user)
 	}
 }
